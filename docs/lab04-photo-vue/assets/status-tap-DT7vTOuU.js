b5349f30a8d35a5ceaf09c2e68b072377342e878
nannpat1997-debug <nannpat1997@gmail.com>
2026-01-11 20:23:16 +0700

    Add lab04-photo-vue built app and update index.html link

---
+++ docs/lab04-photo-vue/assets/status-tap-DT7vTOuU.js
@@ -0,0 +1,3 @@
+import{G as a,H as i,J as c,K as d,L as l}from"./index-D8V3nKhL.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
